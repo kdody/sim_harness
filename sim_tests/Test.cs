fb4b07146266f814ffc870962ef24f4903f70220
Clayton Kuchta <clayton.kuchta@gmail.com>
2015-03-25 18:39:02 -0400

    Initial Commit Device Commands
    - Added device_commands to Hats namespace
    - Added basic test to sim_test/Test for device commands

--- sim_tests/Test.cs
+++ sim_tests/Test.cs
@@ -1,6 +1,9 @@
 ﻿using NUnit.Framework;
 using System;
 using Hats.Time;
+using Hats.device_commands;
+using System.Net.Http;
+
 
 namespace sim_tests
 {
@@ -19,6 +22,37 @@ public class Test
 	{
 		TimeFrame frame = new TimeFrame();
 		Assert.Throws<ArgumentException>(() => frame.setTimeFrame(rate: -1.0));
+	}
+
+	[Test ()]
+	public void CreateDevice()
+	{
+		int houseID = 0;
+		int roomID = 0;
+		HttpClient newClient = new HttpClient();
+		int newID = DeviceCommands.createDevice("devClass", "devType", "devName", houseID, roomID, newClient);
+		Assert.True(newID == 0);
+	}
+
+	[Test ()]
+	public void RemoveDevice()
+	{
+		int houseID = 0;
+		int roomID = 0;
+		HttpClient newClient = new HttpClient();
+		int newID = DeviceCommands.createDevice("devClass", "devType", "devName", houseID, roomID, newClient);
+		bool removed = DeviceCommands.removeDevice(houseID, roomID, newID, newClient);
+		Assert.True(removed == false);
+	}
+	[Test ()]
+	public void ChangeDeviceName()
+	{
+		int houseID = 0;
+		int roomID = 0;
+		HttpClient newClient = new HttpClient();
+		int newID = DeviceCommands.createDevice("devClass", "devType", "devName", houseID, roomID, newClient);
+		bool nameChanged = DeviceCommands.changeDeviceName(houseID, roomID, newID, "devName", newClient);
+		Assert.True(nameChanged == false);
 	}
 }
 }
