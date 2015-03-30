eb94ba4469b17b647e4f193ea1fa547c06bcd90c
Andrew <gandrew2@vt.edu>
2015-03-30 19:38:43 -0400

    Added a class to read and parse a JSON file

---
+++ ConsoleApplication1/reader_test2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hats.Reader;
+
+namespace reader_test2
+{
+    class reader_test2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file name: ");
+            string file = Console.ReadLine();
+            Reader readConfig = new Reader();
+            toVerify toBeChecked = readConfig.readFile(file);
+            foreach(string item in toBeChecked.userNames)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
