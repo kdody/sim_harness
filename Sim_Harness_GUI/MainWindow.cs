a6c775e1ffaba8c8e65cc9a610b5cc6f54891183
Clayton Kuchta <clayton.kuchta@gmail.com>
2015-03-30 13:58:07 -0400

    Initial Commit GUI
    - Added basic Elements of GUI
    - Will add more elements once all logic is in place

---
+++ Sim_Harness_GUI/MainWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Gtk;
+using Sim_Harness_GUI;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		Console.WriteLine("Build");
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Console.WriteLine("Deleted");
+		Application.Quit();
+		a.RetVal = true;
+	}
+
+	protected void OnStartTestButtonClicked (object sender, EventArgs e)
+	{
+		this.testSenarioComboBox.AppendText("Hello");
+	}
+
+	protected void OnNewSenarioCreateButtonClicked (object sender, EventArgs e)
+	{
+		Dialog  newSenario = new New_Senario();
+		int response = newSenario.Run();
+		if(response == (int) Gtk.ResponseType.Ok)
+		{
+			Console.WriteLine("EXIT OKAY");
+		} 
+		else
+		{
+			Console.WriteLine("EXIT CANCEL");
+		}
+
+		newSenario.Destroy();
+	}
+}
---
+++ Sim_Harness_GUI/NewSenario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+namespace Sim_Harness_GUI
+{
+	public partial class New_Senario : Gtk.Dialog
+	{
+		public New_Senario()
+		{
+			this.Build();
+		}
+		protected void OnButtonOkClicked (object sender, EventArgs e)
+		{
+			
+		}
+
+
+		protected void OnButtonCancelClicked (object sender, EventArgs e)
+		{
+		}
+	}
+}
+
